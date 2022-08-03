ce2eda3fd8b608cc43cf6a84b024b94439de7129
Mr Whipp <dwhipp88@gmail.com>
2022-08-03 00:41:39 -0600

    setup database

--- Exercise Files/CH3/03_02/start/app.js
+++ Exercise Files/CH3/03_02/start/app.js
@@ -15,4 +15,22 @@ window.onload = () => {
 
     db = request.result;
   };
+
+  request.onupgradeneeded = function (e) {
+    let db = e.target.result;
+
+    let objectStore = db.createObjectStore('contacts', {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
+
+    objectStore.createIndex('firstName', 'firstName', {
+      unique: false,
+    });
+    objectStore.createIndex('lastName', 'lastName', {
+      unique: false,
+    });
+
+    console.log('database setup complete');
+  };
 };
