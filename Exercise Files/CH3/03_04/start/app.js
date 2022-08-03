7bbfb87df3a9c6a2bf879b928ba822f6b212358b
David Whipple <dwhipp88@gmail.com>
2022-08-03 17:51:07 -0600

    created logic that will add data to the database

--- Exercise Files/CH3/03_04/start/app.js
+++ Exercise Files/CH3/03_04/start/app.js
@@ -4,27 +4,59 @@ const lastNameInput = document.querySelector('#lastName');
 const form = document.querySelector('form');
 
 window.onload = () => {
-    let request = window.indexedDB.open('contacts', 1);
+  let request = window.indexedDB.open('contacts', 1);
 
-    request.onerror = function() {
-        console.log('Database failed to open');
-    }
+  request.onerror = function () {
+    console.log('Database failed to open');
+  };
 
-    request.onsuccess = function() {
-        console.log('Database opened successfully');
-        
-        db = request.result;
-    }
+  request.onsuccess = function () {
+    console.log('Database opened successfully');
 
-    request.onupgradeneeded = function(e) {
-        let db = e.target.result;
+    db = request.result;
+  };
 
-        let objectStore = db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true});
+  request.onupgradeneeded = function (e) {
+    let db = e.target.result;
 
-        objectStore.createIndex('firstName', 'firstName', { unique: false});
-        objectStore.createIndex('lastName', 'lastName', { unique: false});
+    let objectStore = db.createObjectStore('contacts', {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
 
-        console.log('Database setup complete');
-        
-    }
-}
+    objectStore.createIndex('firstName', 'firstName', {
+      unique: false,
+    });
+    objectStore.createIndex('lastName', 'lastName', {
+      unique: false,
+    });
+
+    console.log('Database setup complete');
+  };
+
+  form.onsubmit = addData;
+
+  function addData(e) {
+    e.preventDefault();
+
+    let newItem = {
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value,
+    };
+    let transaction = db.transaction(['contacts'], 'readwrite');
+    let objectStore = transaction.objectStore('contacts');
+    let request = objectStore.add(newItem);
+    request.onsuccess = () => {
+      firstNameInput.value = '';
+      lastNameInput.value = '';
+    };
+
+    transaction.oncomplete = () => {
+      console.log('Transaction completed on the database');
+    };
+
+    transaction.onerror = () => {
+      console.log('Transaction not completed, error!');
+    };
+  }
+};
--- app.js
+++ app.js
@@ -22,3 +22,20 @@ const h1 = document.querySelector('#title');
 name
   ? (h1.textContent = `Welcome ${name}`)
   : (h1.textContent = 'So sad nobody to welcome');
+
+// loclStorage vs sessionStorage
+// when you want to maintain a state of your application,
+// say a shopping cart, and want to control how long the
+// state remains, use localStorage
+// with timers in your code you can set how long the
+// information remains with a persistant localStorage
+// and then remove it whenever you feel is appropriate
+// also, use localStorage for anything that makes the user
+// experience better but DOES NOT compromise user privacy
+
+// when the privacy of users is a concern and you'd like to
+// or need to use web storage, use sessionStorage
+// this way as soon as the user closes their browser, the
+// data is gone
+// sessionStorage is great when the data only needs to be
+// there temporarily
