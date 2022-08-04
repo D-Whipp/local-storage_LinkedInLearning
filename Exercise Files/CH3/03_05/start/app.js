7a59014234bc39130db5e87c428f40bdbb8b7b7e
David Whipple <dwhipp88@gmail.com>
2022-08-03 21:28:37 -0600

    added input logic

--- Exercise Files/CH3/03_05/start/app.js
+++ Exercise Files/CH3/03_05/start/app.js
@@ -2,56 +2,105 @@ let db;
 const firstNameInput = document.querySelector('#firstName');
 const lastNameInput = document.querySelector('#lastName');
 const form = document.querySelector('form');
+const list = document.querySelector('ul');
 
 window.onload = () => {
-    let request = window.indexedDB.open('contacts', 1);
+  let request = window.indexedDB.open('contacts', 1);
 
-    request.onerror = function() {
-        console.log('Database failed to open');
+  request.onerror = function () {
+    console.log('Database failed to open');
+  };
+
+  request.onsuccess = function () {
+    console.log('Database opened successfully');
+
+    db = request.result;
+    displayData();
+  };
+
+  request.onupgradeneeded = function (e) {
+    let db = e.target.result;
+
+    let objectStore = db.createObjectStore('contacts', {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
+
+    objectStore.createIndex('firstName', 'firstName', {
+      unique: false,
+    });
+    objectStore.createIndex('lastName', 'lastName', {
+      unique: false,
+    });
+
+    console.log('Database setup complete');
+  };
+
+  form.onsubmit = addData;
+
+  function addData(e) {
+    e.preventDefault();
+
+    let newItem = {
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value,
+    };
+
+    let transaction = db.transaction(['contacts'], 'readwrite');
+
+    let objectStore = transaction.objectStore('contacts');
+
+    let request = objectStore.add(newItem);
+
+    request.onsuccess = () => {
+      firstNameInput.value = '';
+      lastNameInput.value = '';
+    };
+
+    transaction.oncomplete = () => {
+      console.log('Transaction completed on the database');
+      displayData();
+    };
+
+    transaction.onerror = () => {
+      console.log('Transaction not completed, error!!!');
+    };
+  }
+
+  function displayData() {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
     }
 
-    request.onsuccess = function() {
-        console.log('Database opened successfully');
-        
-        db = request.result;
-    }
+    let objectStore = db
+      .transaction('contacts')
+      .objectStore('contacts');
+    objectStore.openCursor().onsuccess = function (e) {
+      let cursor = e.target.result;
 
-    request.onupgradeneeded = function(e) {
-        let db = e.target.result;
+      if (cursor) {
+        let listItem = document.createElement('li');
+        let first = document.createElement('p');
+        let last = document.createElement('p');
 
-        let objectStore = db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true});
+        listItem.appendChild(first);
+        listItem.appendChild(last);
+        list.appendChild(listItem);
 
-        objectStore.createIndex('firstName', 'firstName', { unique: false});
-        objectStore.createIndex('lastName', 'lastName', { unique: false});
+        first.textContent = cursor.value.firstName;
+        last.textContent = cursor.value.lastName;
 
-        console.log('Database setup complete');
-        
-    }
+        listItem.setAttribute('data-contact-id', cursor.value.id);
 
-    form.onsubmit = addData;
-
-    function addData(e) {
-        e.preventDefault();
-
-        let newItem = { firstName: firstNameInput.value, lastName: lastNameInput.value};
-
-        let transaction = db.transaction(['contacts'], 'readwrite');
-
-        let objectStore = transaction.objectStore('contacts');
-
-        let request = objectStore.add(newItem);
-
-        request.onsuccess = () => {
-            firstNameInput.value = '';
-            lastNameInput.value = '';
-        };
-
-        transaction.oncomplete = () => {
-            console.log('Transaction completed on the database');   
+        cursor.continue();
+      } else {
+        if (!list.firstChild) {
+          let listItem = document.createElement('li');
+          listItem.textContent = 'No contacts store.';
+          list.appendChild(listItem);
         }
-
-        transaction.onerror = () => {
-            console.log('Transaction not completed, error!!!');
-        }
-    }
-}
+      }
+      console.log('Contacts displayed!!!');
+    };
+  }
+};
