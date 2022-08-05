13a3ad98f644c50e9fd66b4c8f38668c0d10309c
David Whipple <dwhipp88@gmail.com>
2022-08-04 18:20:22 -0600

    added delete contact functionality

--- Exercise Files/CH3/03_06/start/app.js
+++ Exercise Files/CH3/03_06/start/app.js
@@ -5,92 +5,128 @@ const form = document.querySelector('form');
 const list = document.querySelector('ul');
 
 window.onload = () => {
-    let request = window.indexedDB.open('contacts', 1);
+  let request = window.indexedDB.open('contacts', 1);
 
-    request.onerror = function() {
-        console.log('Database failed to open');
+  request.onerror = function () {
+    console.log('Database failed to open');
+  };
+
+  request.onsuccess = function () {
+    console.log('Database opened successfully');
+
+    db = request.result;
+    displayData();
+  };
+
+  request.onupgradeneeded = function (e) {
+    let db = e.target.result;
+
+    let objectStore = db.createObjectStore('contacts', {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
+
+    objectStore.createIndex('firstName', 'firstName', {
+      unique: false,
+    });
+    objectStore.createIndex('lastName', 'lastName', {
+      unique: false,
+    });
+
+    console.log('Database setup complete');
+  };
+
+  form.onsubmit = addData;
+
+  function addData(e) {
+    e.preventDefault();
+
+    let newItem = {
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value,
+    };
+
+    let transaction = db.transaction(['contacts'], 'readwrite');
+    let objectStore = transaction.objectStore('contacts');
+    let request = objectStore.add(newItem);
+
+    request.onsuccess = () => {
+      firstNameInput.value = '';
+      lastNameInput.value = '';
+    };
+
+    transaction.oncomplete = () => {
+      console.log('Transaction completed on the database');
+      displayData();
+    };
+
+    transaction.onerror = () => {
+      console.log('Transaction not completed, error!!!');
+    };
+  }
+
+  function displayData() {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
     }
 
-    request.onsuccess = function() {
-        console.log('Database opened successfully');
-        
-        db = request.result;
-        displayData();
-    }
+    let objectStore = db
+      .transaction('contacts')
+      .objectStore('contacts');
+    objectStore.openCursor().onsuccess = function (e) {
+      let cursor = e.target.result;
 
-    request.onupgradeneeded = function(e) {
-        let db = e.target.result;
+      if (cursor) {
+        let listItem = document.createElement('li');
+        let first = document.createElement('p');
+        let last = document.createElement('p');
 
-        let objectStore = db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true});
+        listItem.appendChild(first);
+        listItem.appendChild(last);
+        list.appendChild(listItem);
 
-        objectStore.createIndex('firstName', 'firstName', { unique: false});
-        objectStore.createIndex('lastName', 'lastName', { unique: false});
+        first.textContent = cursor.value.firstName;
+        last.textContent = cursor.value.lastName;
 
-        console.log('Database setup complete');
-        
-    }
+        listItem.setAttribute('data-contact-id', cursor.value.id);
 
-    form.onsubmit = addData;
+        let deleteButton = document.createElement('button');
+        listItem.appendChild(deleteButton);
+        deleteButton.textContent = 'Delete';
 
-    function addData(e) {
-        e.preventDefault();
+        deleteButton.onclick = deleteItem;
 
-        let newItem = { firstName: firstNameInput.value, lastName: lastNameInput.value};
-
-        let transaction = db.transaction(['contacts'], 'readwrite');
-
-        let objectStore = transaction.objectStore('contacts');
-
-        let request = objectStore.add(newItem);
-
-        request.onsuccess = () => {
-            firstNameInput.value = '';
-            lastNameInput.value = '';
-        };
-
-        transaction.oncomplete = () => {
-            console.log('Transaction completed on the database');
-            displayData();  
+        cursor.continue();
+      } else {
+        if (!list.firstChild) {
+          let listItem = document.createElement('li');
+          listItem.textContent = 'No contacts store.';
+          list.appendChild(listItem);
         }
+      }
+      console.log('Contacts displayed!!!');
+    };
+  }
 
-        transaction.onerror = () => {
-            console.log('Transaction not completed, error!!!');
-        }
-    }
+  function deleteItem(e) {
+    let contactId = Number(
+      e.target.parentNode.getAttribute('data-contact-id')
+    );
 
-    function displayData() {
-        while (list.firstChild) {
-            list.removeChild(list.firstChild);
-        }
+    let transaction = db.transaction(['contacts'], 'readwrite');
+    let objectStore = transaction.objectStore('contacts');
+    let request = objectStore.delete(contactId);
 
-        let objectStore = db.transaction('contacts').objectStore('contacts');
-        objectStore.openCursor().onsuccess = function(e) {
-            let cursor = e.target.result;
+    transaction.oncomplete = () => {
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
 
-            if(cursor) {
-                let listItem = document.createElement('li');
-                let first = document.createElement('p');
-                let last = document.createElement('p');
+      console.log(`Contact ${contactId} is deleted`);
 
-                listItem.appendChild(first);
-                listItem.appendChild(last);
-                list.appendChild(listItem);
-
-                first.textContent = cursor.value.firstName;
-                last.textContent = cursor.value.lastName;
-
-                listItem.setAttribute('data-contact-id', cursor.value.id);
-
-                cursor.continue();
-            } else {
-                if(!list.firstChild) {
-                    let listItem = document.createElement('li');
-                    listItem.textContent = 'No contacts store.';
-                    list.appendChild(listItem);
-                }
-            }
-            console.log('Contacts displayed!!!');
-
-        }
-    }
-}
+      if (!list.firstChild) {
+        let listItem = document.createElement('li');
+        listItem.textContent = 'No contacts store.';
+        list.appendChild(listItem);
+      }
+    };
+  }
+};
